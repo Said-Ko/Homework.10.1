2bde84ed73ca5ab5abca4feb2e925410e6b45d81
Саид <said_ccc@mail.ru>
2025-01-21 04:42:06 +0300

    create 3 func

--- example.py
+++ example.py
@@ -1,21 +1,59 @@
-from typing import Dict, Any
+import random
 
-# usd_transactions = filter_by_currency(transactions, "USD")
-# for _ in range(2):
-#     print(next(usd_transactions))
+text = 'XXXX XXXX XXXX XXXX'
+def func1(text):
+    new_text = ''
+    for i in text:
+        if i.isalpha():
+            new_text += str(random.randint(1,9))
+        else:
+            new_text +=i
+    return new_text
 
-print(filter_by_currency({"id": 939719570,
- "state": "EXECUTED",
- "date": "2018-06-30T02:08:58.425572",
- "operationAmount": {
-                     "amount": "9824.07",
-                     "currency": {
-                         "name": "USD",
-                         "code": "USD"
-                     }
-                 },
- "description": "Перевод организации",
- "from": "Счет 75106830613657916952",
- "to": "Счет 11776614605963066702"
- }
-))
+print(func1(text))
+
+import random
+
+text = 'XXXX XXXX XXXX XXXX'
+
+def func2(text):
+    new_text = ''.join(str(random.randint(1,9)) if i.isalpha() else i for i in text)
+    return new_text
+
+print(func2(text))
+
+def func3():
+    unless_numb = '0000 0000 0000 0000'
+    new_code = ''
+    for numb in unless_numb:
+        if numb.isdigit():
+            new_code += str(random.randint(1, 9))
+            # numb.replace(str(random.randint(1, 9)))
+        else:
+            new_code +=numb
+    return new_code
+
+print(func3())
+
+# def random_number_generator(start, stop):
+#     while True:
+#         yield random.randint(start, stop)
+
+
+# def func1(start, stop):
+#     a = []
+#     while len(a) < 19:
+#         if len(a) % 4 == 0:
+#             a.append(' ')
+#         elif True:
+#             a.append(random.randint(start, stop+1))
+#
+# print(func1(1,9))
+
+# def random_number_generator(start, stop):
+#     while True:
+#         yield random.randint(start, stop)
+#
+# print(next(random_number_generator(8, 12)))
+# print(next(random_number_generator(8, 12)))
+# print(next(random_number_generator(8, 12)))
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,8 +1,21 @@
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
-from typing import List, Dict, Any
+# from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+from src.generators import transaction_descriptions,filter_by_currency
+from tests.conftest import filter_by_currency_fixture_one,filter_by_currency_fixture_two
 import pytest
 
 
-@pytest.fixture()
-def test_filter_by_currency(transactions: Any) -> None:
-    filter_by_currency(transactions) == currency
+def test_filter_by_currency_fixture(filter_by_currency_verification: list) -> None:
+    assert next(filter_by_currency(filter_by_currency_verification, "USD")) == filter_by_currency_fixture_one
+    assert next(filter_by_currency(filter_by_currency_verification, "RUB")) == filter_by_currency_fixture_two
+
+
+def test_transaction_descriptions():
+    result_test_transaction_descriptions = transaction_descriptions()
+    assert next(result_test_transaction_descriptions) == "Перевод организации"
+    assert next(result_test_transaction_descriptions) == "Перевод со счета на счет"
+    assert next(result_test_transaction_descriptions) == "Перевод со счета на счет"
+    assert next(result_test_transaction_descriptions) == "Перевод с карты на карту"
+    assert next(result_test_transaction_descriptions) == "Перевод организации"
+
+# def test_card_number_generator():
+#     assert next()
