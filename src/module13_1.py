9345e027744b2798aa81128993d771cdca9a28a3
Саид <said_ccc@mail.ru>
2025-05-20 00:38:35 +0300

    функиции модуля 13_1 рабочие, приступаем к тестам

--- src/module13_1.py
+++ src/module13_1.py
@@ -1,24 +1,15 @@
+import os
 import csv
-# import pandas as pd
+import pandas as pd
+
+script_dir = os.path.dirname(__file__)
+project_root = os.path.dirname(script_dir)  # Папка HomeWork_9.1
+transaction_csv = os.path.join(project_root, 'transaction', 'transactions.csv')
+transaction_xlsx = os.path.join(project_root, 'transaction', 'transactions_excel.xlsx')
 
 
-transaction_csv = 'transaction/transactions.csv'
-
-
-# Обратока CSV
-
-# with open(transaction_csv, encoding='utf-8') as file:
-#     reader = csv.DictReader(file, delimiter=";")
-#     for row in reader:
-#
-#         print(row)
-
-
-
-
-
-
-def read_csv_transactions(file_csv):
+# # обработка данных CSV формата
+def read_csv_transactions(file_csv: list[dict]) -> list[dict]:
     """функция считывания транзакции .CSV формата"""
     transactions = []
     try:
@@ -26,12 +17,22 @@ def read_csv_transactions(file_csv):
             reader = csv.DictReader(file, delimiter=";")
             for row in reader:
                 transactions.append(row)
-                # print(row)
             return transactions
-    except Exception:
-        return []
-#
-#
-print(read_csv_transactions(transaction_csv))
+    except FileNotFoundError:
+        return (f"Файл не найден: {file_csv}")
 
-# transaction_xlsx = 'transaction/transactions.xlsx'
+
+# print(read_csv_transactions(transaction_csv))
+
+
+# обработка данных .xlsx формата
+def read_xlsx_transactions(file_xlsx):
+    """функция считывания транзакции .xlsx формата"""
+    try:
+        df = pd.read_excel(file_xlsx)
+        xlsx_transactions = df.to_dict('records')
+        return xlsx_transactions
+    except Exception:
+        return (f'Файл не найден: {file_xlsx}')
+
+# print(read_xlsx_transactions(transaction_xlsx))
